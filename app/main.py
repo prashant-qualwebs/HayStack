bb53953e9130768b38b89f445acce160e09db6c8
iamprashantverma <prashant2k26@gmail.com>
2026-04-16 12:30:26 +0530

    refactoe:project title removed

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from app.core.config import settings
 from app.core.middleware import setup_middleware
 from app.api.v1.router import api_router
 
-app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
+app = FastAPI()
 
 setup_middleware(app)
-app.include_router(api_router, prefix=settings.API_V1_STR)
 
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 @app.get("/")
 async def root():
